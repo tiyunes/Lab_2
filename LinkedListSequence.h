a52bc60985a4e0c7c2bf38005c5fd4f198202e7c
tiyunes <79756733+tiyunes@users.noreply.github.com>
2021-05-05 10:29:58 +0300

    Update LinkedListSequence.h

--- LinkedListSequence.h
+++ LinkedListSequence.h
@@ -13,6 +13,7 @@ public:
     {
         this->items = new LinkedList<T>();
     }
+    
     LinkedListSequence(T* items, int c)
     {
         this->items = new LinkedList<T>(items, c);
@@ -32,44 +33,54 @@ public:
     {
         return this->items->GetFirst();
     }
+    
     T GetLast() override
     {
         return this->items->GetLast();
     }
+    
     T Get(int index) override
     {
         return this->items->Get(index);
     }
+    
     int GetLength() override
     {
         return this->items->GetLength();
     }
+    
     void Append(T item) override
     {
         this->items->Append(item);
     }
+    
     void Prepend(T item) override
     {
         this->items->Prepend(item);
     }
+    
     void InsertAt(T item, int index) override
     {
         this->items->InsertAt(item, index);
     }
+    
     void Pop() override
     {
         return this->items->Pop();
     }
+    
     void Reverse() override
     {
         return this->items->Reverse();
     }
+    
     Sequence<T>* GetSubsequence(int startIndex, int endIndex) override
     {
         LinkedListSequence<T>* subList = new LinkedListSequence<T>(*(this->items->GetSubList(startIndex, endIndex)));
         subList->items->Size = endIndex - startIndex + 1;
         return subList;
     }
+    
     Sequence<T>* Concat(Sequence <T> *l) override
     {
         Sequence<T>* concatSequence = (Sequence<T>*)new LinkedListSequence<T>();
@@ -83,6 +94,7 @@ public:
         }
         return concatSequence;
     }
+    
     ~LinkedListSequence()
     {
         delete[] this->items;
